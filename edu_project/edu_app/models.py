501c4f1fc35f57fd4d68fc9302a1880e8e2e201d
Owen Campbell-Moore <owencmoore@gmail.com>
2014-01-22 23:40:47 +0100

    First commit

---
+++ edu_project/edu_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class SubjQuestion(models.Model):
+	question_text = models.CharField(max_length=200, default='What is the answer?')
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.question_text
+
+class SubjAnswer(models.Model):
+	question = models.ForeignKey(SubjQuestion)
+	answer_text = models.TextField(default='This is the answer')
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.answer_text
+
+class SubjAnswerEvaluation(models.Model):
+	answer = models.ForeignKey(SubjAnswer)
+	score = models.FloatField(default=0.5)
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.score
